5f008ca59b3222ecf9292d3c8a886d5acb41bd18
leandrolm2 <leandromelgarejo@hotmail.com>
2021-08-28 14:22:12 -0300

    small changes

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ const play2 = {
 }
 let gamer = {}
 
-console.log(`|${a}|\n|${b}|\n|${c}|\n`);
 
 const aboutPlayer = () =>{
   rl.question("what's your name player 1? ", function (name1) {
@@ -41,102 +40,12 @@ const aboutPlayer = () =>{
   rl.prompt
 } //get the name of the players
 
-const winner = () => {
-  const rematch = () => {
-    rl.question('Wanna play again? (yes/no): ', function(reloaded){
-      switch (reloaded) {
-        case 'yes':
-          console.log('NICE! SO...');
-          play1.numb = 2
-          play2.numb = 1
-          
 
-          for (i = 0; i <a.length;i++){
-            a[i] = '-'
-            b[i] = '-'
-            c[i] = '-'
-          }
-
-          game()
-          break;
-        case 'no':
-          if (play1.points > play2.points){
-            console.log(`With ${play1.points} points, ${play1.name} was the overall winner of the match over ${play2.name}, with ${play2.points} poinst`)
-            process.exit()
-
-          } else if (play2.points > play1.points){
-            console.log(`With ${play2.points} points, ${play2.name} was the overall winner of the match over ${play1.name}, with ${play1.points} poinst`)
-            process.exit()
-
-          } else {
-            console.log(`The overall match was an empate, both players with the amount of ${play1.points}`)
-            process.exit()
-          }
-          break;
-        default:
-          console.log(`${reloaded} it's not a valid option, please say 'yes' or 'no'`);
-          rematch()
-      }
-    });
-
-  } //reset the ga
-
-  if (a[0] === gamer.weapon && a[1] === gamer.weapon && a[2] === gamer.weapon){
-    console.log(`YOU WON ${gamer.name}!!`);
-    gamer.points += 2
-    rematch();
-    
-  } else if(b[0] === gamer.weapon && b[1] === gamer.weapon && b[2] === gamer.weapon){
-    console.log(`YOU WON ${gamer.name}!!`)
-    gamer.points += 2
-    rematch()
-
-  } else if(c[0] === gamer.weapon && c[1] === gamer.weapon && c[2] === gamer.weapon){
-    console.log(`YOU WON ${gamer.name}!!`)
-    gamer.points += 2
-    rematch()
-
-  } else if(a[0] === gamer.weapon && b[0] === gamer.weapon && c[0] === gamer.weapon){
-    console.log(`YOU WON ${gamer.name}!!`)
-    gamer.points += 2
-    rematch()
-
-  } else if(a[1] === gamer.weapon && b[1] === gamer.weapon && c[1] === gamer.weapon){
-    console.log(`YOU WON ${gamer.name}!!`)
-    gamer.points += 2
-    rematch()
-
-  } else if(a[2] === gamer.weapon && b[2] === gamer.weapon && c[2] === gamer.weapon){
-    console.log(`YOU WON ${gamer.name}!!`)
-    gamer.points += 2
-    rematch()
-
-  } else if(a[0] === gamer.weapon && b[1] === gamer.weapon && c[2] === gamer.weapon){
-    console.log(`YOU WON ${gamer.name}!!`)
-    gamer.points += 2
-    rematch()
-
-  } else if(a[2] === gamer.weapon && b[1] === gamer.weapon && c[0] === gamer.weapon){
-    console.log(`YOU WON ${gamer.name}!!`)
-    gamer.points += 2
-    rematch()
-
-  } else if(!a.includes('-') && !b.includes('-') && !c.includes('-')){
-    console.log('So close! The game has empate')
-    play1.points += 1
-    play2.points += 1
-    rematch()
-
-  } else {
-    game()
-  }
-} // The mechanic checks whether the player has won and if they want to continue playing
 
 aboutPlayer()
 
 const game = () => {
-  
-  
+   
   const round = () => {
     if (play1.numb % 2 == 0){
       gamer = play1
@@ -152,11 +61,13 @@ const game = () => {
       play2.numb += 1
     }
   }// Define witch player will gonna play
+
     
   rl.question(`Please, chose a letter between "a,b and c":  `, function (letter) {
     rl.question(`Know chose a number between 0 and 2: `, function(numb){
 
       if (!letter.match(/^(a|b|c)$/) || !numb.match(/^(0|1|2)$/)){
+        console.clear()
         console.log('Chose a valid letter or number')
         game()
 
@@ -170,6 +81,7 @@ const game = () => {
 
           } else {
             round()
+            console.clear()
             console.log(`${gamer.name}, That was your move`)
             a[parseInt(numb)] = gamer.weapon;
             console.log(`|${a}|\n|${b}|\n|${c}|\n`);
@@ -184,6 +96,7 @@ const game = () => {
 
           } else {
             round()
+            console.clear()
             console.log(`${gamer.name}, That was your move`)
             b[parseInt(numb)] = gamer.weapon;
             console.log(`|${a}|\n|${b}|\n|${c}|\n`);
@@ -198,6 +111,7 @@ const game = () => {
 
           } else {
             round()
+            console.clear()
             console.log(`${gamer.name}, That was your move`);
             c[parseInt(numb)] = gamer.weapon;
             console.log(`|${a}|\n|${b}|\n|${c}|\n`);
@@ -211,7 +125,91 @@ const game = () => {
     rl.prompt();
   }); //the mechanic that display the "X" or "O", depends on which player is playing
 
-  aboutPlayer()
+  const winner = () => {
+    const rematch = () => {
+      console.log(`YOU WON ${gamer.name}!!`);
+      gamer.points += 2
+
+      rl.question('Wanna play again? (yes/no): ', function(reloaded){
+        switch (reloaded) {
+          case 'yes':
+            console.log('NICE! SO...');
+            play1.numb = 2
+            play2.numb = 1
+            
+  
+            for (i = 0; i <a.length;i++){
+              a[i] = '-'
+              b[i] = '-'
+              c[i] = '-'
+            }
+  
+            game()
+            break;
+          case 'no':
+            if (play1.points > play2.points){
+              console.log(`With ${play1.points} points, ${play1.name} was the overall winner of the match over ${play2.name}, with ${play2.points} poinst`)
+              process.exit()
+  
+            } else if (play2.points > play1.points){
+              console.log(`With ${play2.points} points, ${play2.name} was the overall winner of the match over ${play1.name}, with ${play1.points} poinst`)
+              process.exit()
+  
+            } else {
+              console.log(`The overall match was an empate, both players with the amount of ${play1.points}`)
+              process.exit()
+            }
+            break;
+          default:
+            console.log(`${reloaded} it's not a valid option, please say 'yes' or 'no'`);
+            rematch()
+        }
+      });
+  
+    } //reset the game and decide the winner between players
+  
+    if (a[0] === gamer.weapon && a[1] === gamer.weapon && a[2] === gamer.weapon){
+      
+      rematch();
+      
+    } else if(b[0] === gamer.weapon && b[1] === gamer.weapon && b[2] === gamer.weapon){
+    
+      rematch()
+  
+    } else if(c[0] === gamer.weapon && c[1] === gamer.weapon && c[2] === gamer.weapon){
+      
+      rematch()
+  
+    } else if(a[0] === gamer.weapon && b[0] === gamer.weapon && c[0] === gamer.weapon){
+     
+      rematch()
+  
+    } else if(a[1] === gamer.weapon && b[1] === gamer.weapon && c[1] === gamer.weapon){
+      
+      rematch()
+  
+    } else if(a[2] === gamer.weapon && b[2] === gamer.weapon && c[2] === gamer.weapon){
+      
+      rematch()
+  
+    } else if(a[0] === gamer.weapon && b[1] === gamer.weapon && c[2] === gamer.weapon){
+      
+      rematch()
+  
+    } else if(a[2] === gamer.weapon && b[1] === gamer.weapon && c[0] === gamer.weapon){
+      
+      rematch()
+  
+    } else if(!a.includes('-') && !b.includes('-') && !c.includes('-')){
+      console.log('So close! The game has empate')
+      play1.points += 1
+      play2.points += 1
+      rematch()
+  
+    } else {
+      game()
+    }
+  } // The mechanic checks whether the player has won and if they want to continue playing
 };
 
 rl.on("close", function () {
